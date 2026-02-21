53ef010de5abd9e813a44cc8950d708641bebe5b
Silent Twin <amitybell@silenttwin.net>
2026-02-21 17:53:03 +0000

    Misc quality-of-life improvements
    - reduce min Firefox version to 140 so ESR is included
    - use pageAction icons to show the countdown
      this works better on mobile where the browser action button isn't visible
    - reduce max countdown to 10s
    - restrict the request handler to `main_frame`

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
-const MIN_DELAY = 5_000;
-const MAX_DELAY = 20_000;
-const BACKOFF = 1.25;
-const TICK_TIMEOUT = 500;
-const COUNTDOWN_DECREMENT = 500;
+const MIN_DELAY = 3_000;
+const MAX_DELAY = 10_000;
+const BACKOFF = 1000;
+const TICK_TIMEOUT = 1000;
+const COUNTDOWN_DECREMENT = 1000;
+const MIN_ICON_NUM = 1;
+const MAX_ICON_NUM = 10;
 class State {
     tabId;
     delay = MIN_DELAY;
@@ -21,24 +23,21 @@ class State {
         this.stop();
         this.countdown = this.delay;
         this.timeout = setTimeout(() => this.#tick(), TICK_TIMEOUT);
-        this.delay = Math.min(MAX_DELAY, this.delay * BACKOFF);
+        this.delay = Math.min(MAX_DELAY, this.delay + BACKOFF);
     }
     #tick() {
-        this.countdown -= COUNTDOWN_DECREMENT;
         if (this.countdown <= 0) {
             refreshTab(this.tabId);
             return;
         }
-        browser.browserAction.setBadgeText({
-            tabId: this.tabId,
-            text: `${(this.countdown / 1000).toFixed(1)}s`,
-        });
+        render({ tabId: this.tabId, countdown: this.countdown });
+        this.countdown -= COUNTDOWN_DECREMENT;
         this.timeout = setTimeout(() => this.#tick(), TICK_TIMEOUT);
     }
 }
 const states = {};
 const retryStatusCodes = new Set([403, 429]);
-browser.webRequest.onResponseStarted.addListener(({ statusCode, method, url, tabId }) => {
+browser.webRequest.onResponseStarted.addListener(({ statusCode, method, tabId }) => {
     if (retryStatusCodes.has(statusCode) && method === "GET") {
         resetState(tabId);
     }
@@ -47,6 +46,7 @@ browser.webRequest.onResponseStarted.addListener(({ statusCode, method, url, tab
     }
 }, {
     urls: ["<all_urls>"],
+    types: ["main_frame"],
 });
 const deleteState = (tabId) => {
     states[tabId]?.stop();
@@ -58,8 +58,30 @@ const resetState = (tabId) => {
 };
 const refreshTab = async (tabId) => {
     states[tabId]?.stop();
-    browser.browserAction.setBadgeText({ tabId, text: "" });
+    render({ tabId });
     browser.tabs.reload(tabId, { bypassCache: true });
+};
+const render = async (props) => {
+    const { tabId, countdown } = props;
+    const show = !!countdown;
+    const title = show ? `${(countdown / 1000).toFixed(1)}s` : null;
+    const icon = show ? iconPath(countdown / 1000) : null;
+    const res = await Promise.allSettled([
+        browser.browserAction.setBadgeText({ tabId, text: title }),
+        browser.pageAction.setTitle({ tabId, title }),
+        icon && browser.pageAction.setIcon({ tabId, path: icon }),
+    ]);
+    res.forEach((r) => {
+        if (r.status === "rejected") {
+            console.error("ReTry: render error:", r.reason);
+        }
+    });
+    if (show) {
+        browser.pageAction.show(tabId);
+    }
+    else {
+        browser.pageAction.hide(tabId);
+    }
 };
 const refreshActiveTab = async () => {
     const tabs = await browser.tabs.query({ active: true });
@@ -69,4 +91,18 @@ const refreshActiveTab = async () => {
         }
     });
 };
-browser.browserAction.onClicked.addListener(refreshActiveTab);
+const iconPath = (n) => {
+    n = Math.ceil(n);
+    return MIN_ICON_NUM <= n && n <= MAX_ICON_NUM
+        ? `icons/${n}.svg`
+        : `icons/icon.svg`;
+};
+const main = async () => {
+    browser.browserAction.onClicked.addListener(refreshActiveTab);
+    browser.pageAction.onClicked.addListener((tab) => {
+        if (tab.id) {
+            refreshTab(tab.id);
+        }
+    });
+};
+main();
--- background.ts
+++ background.ts
@@ -1,8 +1,10 @@
-const MIN_DELAY = 5_000;
-const MAX_DELAY = 20_000;
-const BACKOFF = 1.25;
-const TICK_TIMEOUT = 500;
-const COUNTDOWN_DECREMENT = 500;
+const MIN_DELAY = 3_000;
+const MAX_DELAY = 10_000;
+const BACKOFF = 1000;
+const TICK_TIMEOUT = 1000;
+const COUNTDOWN_DECREMENT = 1000;
+const MIN_ICON_NUM = 1;
+const MAX_ICON_NUM = 10;
 
 class State {
   tabId: number;
@@ -25,20 +27,17 @@ class State {
     this.stop();
     this.countdown = this.delay;
     this.timeout = setTimeout(() => this.#tick(), TICK_TIMEOUT);
-    this.delay = Math.min(MAX_DELAY, this.delay * BACKOFF);
+    this.delay = Math.min(MAX_DELAY, this.delay + BACKOFF);
   }
 
   #tick() {
-    this.countdown -= COUNTDOWN_DECREMENT;
     if (this.countdown <= 0) {
       refreshTab(this.tabId);
       return;
     }
 
-    browser.browserAction.setBadgeText({
-      tabId: this.tabId,
-      text: `${(this.countdown / 1000).toFixed(1)}s`,
-    });
+    render({ tabId: this.tabId, countdown: this.countdown });
+    this.countdown -= COUNTDOWN_DECREMENT;
     this.timeout = setTimeout(() => this.#tick(), TICK_TIMEOUT);
   }
 }
@@ -48,7 +47,7 @@ const states: { [tabId: number]: State } = {};
 const retryStatusCodes = new Set([403, 429]);
 
 browser.webRequest.onResponseStarted.addListener(
-  ({ statusCode, method, url, tabId }) => {
+  ({ statusCode, method, tabId }) => {
     if (retryStatusCodes.has(statusCode) && method === "GET") {
       resetState(tabId);
     } else {
@@ -57,6 +56,7 @@ browser.webRequest.onResponseStarted.addListener(
   },
   {
     urls: ["<all_urls>"],
+    types: ["main_frame"],
   },
 );
 
@@ -72,8 +72,38 @@ const resetState = (tabId: number) => {
 
 const refreshTab = async (tabId: number) => {
   states[tabId]?.stop();
-  browser.browserAction.setBadgeText({ tabId, text: "" });
+  render({ tabId });
   browser.tabs.reload(tabId, { bypassCache: true });
+};
+
+type RenderProps = {
+  tabId: number;
+  countdown?: number;
+};
+
+const render = async (props: RenderProps) => {
+  const { tabId, countdown } = props;
+
+  const show = !!countdown;
+  const title = show ? `${(countdown / 1000).toFixed(1)}s` : null;
+  const icon = show ? iconPath(countdown / 1000) : null;
+
+  const res = await Promise.allSettled([
+    browser.browserAction.setBadgeText({ tabId, text: title }),
+    browser.pageAction.setTitle({ tabId, title }),
+    icon && browser.pageAction.setIcon({ tabId, path: icon }),
+  ]);
+  res.forEach((r) => {
+    if (r.status === "rejected") {
+      console.error("ReTry: render error:", r.reason);
+    }
+  });
+
+  if (show) {
+    browser.pageAction.show(tabId);
+  } else {
+    browser.pageAction.hide(tabId);
+  }
 };
 
 const refreshActiveTab = async () => {
@@ -85,4 +115,20 @@ const refreshActiveTab = async () => {
   });
 };
 
-browser.browserAction.onClicked.addListener(refreshActiveTab);
+const iconPath = (n: number) => {
+  n = Math.ceil(n);
+  return MIN_ICON_NUM <= n && n <= MAX_ICON_NUM
+    ? `icons/${n}.svg`
+    : `icons/icon.svg`;
+};
+
+const main = async () => {
+  browser.browserAction.onClicked.addListener(refreshActiveTab);
+  browser.pageAction.onClicked.addListener((tab) => {
+    if (tab.id) {
+      refreshTab(tab.id);
+    }
+  });
+};
+
+main();
